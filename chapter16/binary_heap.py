c57c5f20207854d9256599a145cb389bc03e1012
domssilva <domenique_silva@hotmail.com>
2022-02-14 20:53:13 +0100

    implement: binary heap deletion

--- chapter16/binary_heap.py
+++ chapter16/binary_heap.py
@@ -33,7 +33,40 @@ class Binary_Heap:
             # update inserted node idx
             new_node_idx = self.parent_idx(new_node_idx)
             parent_idx = self.parent_idx(new_node_idx)
-            
+
+    def delete(self):
+            # Remove last element of heap and place it as the new root
+            self.data[0] = self.data.pop()
+            # trickle node down
+            trickle_node_idx = 0
+            while self.has_greater_child(trickle_node_idx):
+                # save larger child index in variable
+                larger_child_idx = self.calculate_larger_child_idx(trickle_node_idx)
+                self.swap(trickle_node_idx, larger_child_idx)
+                trickle_node_idx = larger_child_idx
+
+    def has_greater_child(self, idx):
+        left = self.left_child_idx(idx)
+        right = self.right_child_idx(idx)
+        # check if indexes are in range to avoid an exception
+        if left < len(self.data) and right < len(self.data):
+            return (
+                (self.data[left] and self.data[left] > self.data[idx]) or 
+                (self.data[right] and self.data[right] > self.data[idx])
+                )
+        return False
+
+    def calculate_larger_child_idx(self, idx):
+        # if there is no right child
+        if not self.data[self.right_child_idx(idx)]:
+            return self.left_child_idx(idx)
+        # if right child is greater than left child
+        if self.data[self.right_child_idx(idx)] > self.data[self.left_child_idx(idx)]:
+            return self.right_child_idx(idx)
+        # if left child is greater than or equal to right child
+        else:
+            return self.left_child_idx(idx)
+
 
 heap = Binary_Heap()
 heap.insert(100)
@@ -48,8 +81,7 @@ heap.insert(50)
 heap.insert(2)
 heap.insert(15)
 heap.insert(3)
-heap.insert(140)
 
+heap.delete()
 print('root:', heap.get_root())
 print('last node:', heap.get_last())
-print('Data:', heap.data)
